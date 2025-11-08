2d3a73061688845f673ec55b612e23e177165e07
tah5882 <me@tah5882.dev>
2025-11-08 17:53:11 +0900

    fix: h-fullクラスをhtml要素に追加し、bodyのh-screenクラスを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-bl from-[#FBAFB7] via-[#E7D1D9] to-[#A8EAEF] h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-bl from-[#FBAFB7] via-[#E7D1D9] to-[#A8EAEF]`}
       >
         {children}
       </body>
