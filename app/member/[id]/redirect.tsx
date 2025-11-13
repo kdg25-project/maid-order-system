352b3726d25a18ef22940ca255c1f2c406d6a2bb
aomona <yuka41623@gmail.com>
2025-11-13 16:31:03 +0900

    feat/Route members by status

--- app/member/[id]/redirect.tsx
+++ app/member/[id]/redirect.tsx
@@ -7,7 +7,27 @@ export default function RedirectPage(props: { userdata: User }) {
   localStorage.setItem("userId", props.userdata.id);
   const router = useRouter();
   useEffect(() => {
-    if (props.userdata.status === "welcome") router.push("/welcome");
+    switch (props.userdata.status) {
+      case "welcome":
+        router.push("/welcome");
+        break;
+      case "order":
+        router.push("/order");
+        break;
+      case "eating":
+        router.push("/order/success");
+        break;
+      case "instax_waiting":
+      case "instax_draw":
+      case "instax_complete":
+      case "leaving":
+        window.location.assign(
+          `https://instax.kdgn.tech/picture/${localStorage.getItem("userId")})`,
+        );
+        break;
+      default:
+        router.push("/");
+    }
   }, [router, props.userdata.status]);
   return null;
 }
