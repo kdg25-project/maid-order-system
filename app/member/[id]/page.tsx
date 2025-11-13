865c3a2b2572b2d2f97831b1c0d55eb5142ca1e0
aomona <yuka41623@gmail.com>
2025-11-13 17:40:08 +0900

    Call next/navigation notFound for missing user

--- app/member/[id]/page.tsx
+++ app/member/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getUserById } from "@/api/users";
 import RedirectPage from "./redirect";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
@@ -9,7 +10,7 @@ export default async function Page({
   const { id } = await params;
   const res = await getUserById(id);
   if (res.data.success === false) {
-    return <div>error:ユーザーが見つかりません。</div>;
+    notFound();
   }
   const data = res.data.data;
   return <RedirectPage userdata={data} />;
