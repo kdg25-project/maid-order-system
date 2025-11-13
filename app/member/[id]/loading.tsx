0860acd01ca6d39bede6abc71ea7324b7c912d5a
aomona <yuka41623@gmail.com>
2025-11-13 15:49:13 +0900

    Show spinner on member loading screen

--- app/member/[id]/loading.tsx
+++ app/member/[id]/loading.tsx
@@ -1,3 +1,10 @@
+import { Spinner } from "@/components/ui/spinner";
+
 export default function Loading() {
-  return <p>Loading...</p>;
+  return (
+    <p className="flex flex-row items-center justify-center mt-20 text-lg text-gray-600 gap-2">
+      Loading...
+      <Spinner />
+    </p>
+  );
 }
